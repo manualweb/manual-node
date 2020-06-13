29dae56fd39bec53a014fffda372c6f97c474d96
Víctor Cuervo <vcuervo@gmail.com>
2020-06-13 13:47:56 +0200

    Ejemplo de uso de módulos en NodeJS

---
+++ modules/sample-module.js
@@ -0,0 +1,7 @@
+//console.log('Ejecutando el módulo de prueba');
+
+function saludo() {
+    console.log("Hola Mundo");
+}
+
+module.exports = { saludo }
---
+++ modules/use-module.js
@@ -0,0 +1,5 @@
+console.log('Ejecuto el Programa Principal');
+
+const lib = require('./sample-module.js')
+lib.saludo();
+console.log('Fin del Programa Principla');
